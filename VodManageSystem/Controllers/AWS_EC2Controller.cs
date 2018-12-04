c868ca3392bbd9dc3cfaf082edf21fe87b4a85f5
chaolee <leechao1959@gmail.com>
2018-12-04 15:26:08 -0500

    Moved the technical documents file to wwwroot folder

--- VodManageSystem/Controllers/AWS_EC2Controller.cs
+++ VodManageSystem/Controllers/AWS_EC2Controller.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Html;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -13,19 +14,23 @@ namespace VodManageSystem.Controllers
 {
     public class AWS_EC2Controller : Controller
     {
-        // GET: /<controller>/
-        public IActionResult Index()
+        private readonly IHostingEnvironment _hostingEnvironment;
+
+        public AWS_EC2Controller(IHostingEnvironment hostingEnvironment)
         {
-            return View();
+            _hostingEnvironment = hostingEnvironment;
         }
 
         // GET: /<controller>/
-        public IActionResult Index_HtmlString()
+        public IActionResult Index()
         {
             string resultRead  = string.Empty;
             try
             {   // Open the text file using a stream reader.
-                string fileName = @"./Views/AWS_EC2/AWS_EC2_Document.html";
+                // string fileName = @"wwwroot/TechDocuments/AWS_EC2_Document.html";
+                string wwwrootPath = _hostingEnvironment.WebRootPath;
+                Console.WriteLine("wwwrootPath = " + wwwrootPath);
+                string fileName = wwwrootPath + "/TechDocuments/AWS_EC2/AWS_EC2_Document.html";
                 using (StreamReader sr = new StreamReader(fileName))
                 {
                     // Read the stream to a string, and write the string to the console.
